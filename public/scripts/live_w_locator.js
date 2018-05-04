c4a8b404440897c918149cdffea83421d22e2579
DKrieger97 <38888835+DKrieger97@users.noreply.github.com>
2018-05-04 10:40:44 +0700

    live_w_locator

--- public/scripts/live_w_locator.js
+++ public/scripts/live_w_locator.js
@@ -1,26 +1,4 @@
 $(function() {
-    var resultCollector = Quagga.ResultCollector.create({
-        capture: true,
-        capacity: 20,
-        blacklist: [{
-            code: "WIWV8ETQZ1", format: "code_93"
-        }, {
-            code: "EH3C-%GU23RK3", format: "code_93"
-        }, {
-            code: "O308SIHQOXN5SA/PJ", format: "code_93"
-        }, {
-            code: "DG7Q$TV8JQ/EN", format: "code_93"
-        }, {
-            code: "VOFD1DB5A.1F6QU", format: "code_93"
-        }, {
-            code: "4SO64P4X8 U4YUU1T-", format: "code_93"
-        }],
-        filter: function(codeResult) {
-            // only store results which match this constraint
-            // e.g.: codeResult
-            return true;
-        }
-    });
     var App = {
         init: function() {
             var self = this;
