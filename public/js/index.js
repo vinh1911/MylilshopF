e783910c8bac54a0f7debee007aeafdad9af39e0
vinh1911 <38676339+vinh1911@users.noreply.github.com>
2018-05-06 21:16:07 +0700

    :)

--- public/js/index.js
+++ public/js/index.js
@@ -9,6 +9,8 @@ var config = {
 };
 firebase.initializeApp(config);
 
+
+//Login check
 firebase.auth().onAuthStateChanged(function(user) {
   if (user) {
 	  
@@ -23,21 +25,33 @@ function logout(){
   firebase.auth().signOut();
 }
 
-
-var database = firebase.database();
+// Add products
+var rootRef = firebase.database();
 var itemCode = document.getElementById("itemCode");
 var itemName = document.getElementById("itemName");
 var itemQuantity = document.getElementById("itemQuantity");
-var itemPrice = document.getElementById("itemCode");
+var itemPrice = document.getElementById("itemPrice");
 function submitClick() {
 	var iCode = itemCode.value;
 	var iName = itemName.value;
 	var iPrice = itemPrice.value;
 	var iQuantity = itemQuantity.value;
-  firebase.database().ref('Inventory/' + iCode).set({
+	rootRef.ref('Inventory/' + iCode).set({
+	Code: iCode,
     Name: iName,
 	Price: iPrice,
 	Quantity: iQuantity
   });
 }
 
+// Display products
+var invRef = rootRef.ref().child("Inventory");
+
+invRef.on("child_added", snap => {
+	var code = snap.child("Code").val();
+	var name = snap.child("Name").val();
+	var price = snap.child("Price").val();
+	var quantity = snap.child("Quantity").val();
+	
+	$("#table_body").append("<tr><td>" + code + "</td><td>" + name + "</td><td>" + price +"</td><td>"+quantity+"</td><tr>");
+});
