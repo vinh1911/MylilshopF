373e49ab0d48351f77c2169dce5ee9f016764a0c
vinh1911 <38676339+vinh1911@users.noreply.github.com>
2018-05-10 13:55:53 +0700

    show data on shop and user html

--- public/js/index.js
+++ public/js/index.js
@@ -15,12 +15,29 @@ firebase.auth().onAuthStateChanged(function(user) {
         	var price = snap.child("Price").val();
         	var cost = snap.child("Cost").val();
         	var quantity = snap.child("Shop").child(currentshop).val();
-        	$("#table_body").append("<tr><td>" + code + "</td><td>" + name + "</td><td>" + cost +"</td><td>"+price+"</td><td>"+quantity+"</td><tr>");
+        	$("#table_body").append("<tr><td>" + code + "</td><td>" + name + "</td><td>" + cost +"</td><td>"+price+"</td><td>"+quantity+"</td><td><button class='btn btn-primary btn-sm' onclick='fillcode("+code+");'>←</button></td></tr>");
+        });
+      }
+      else if (page == "addUser.html"){
+        uroot.on("child_added", snap => {
+          $("#user_body").append("<tr><td>" + snap.val().Name + "</td><td>" + snap.val().Email + "</td><td>" + snap.val().ShopID +"</td><td>"+snap.val().Role+"</td></tr>");
+        });
+      }
+      else if (page == "addShop.html"){
+        var sroot = firebase.database().ref().child("Shops");
+        sroot.on("child_added", snap => {
+          $("#shop_body").append("<tr id=de_"+snap.key+"><td>" + snap.key + "</td><td>" + snap.val() + "</td></tr>");
         });
       }
     });
   }
 });
+
+
+
+
+
+
 
 function logoutClick(){
   firebase.auth().signOut();
@@ -75,24 +92,82 @@ function submitClick() {
 // Display products
 
 
+
+var items;
 var sum = 0;
 var row=0;
 function bcsubmit() {
   var key = document.getElementById('barcode').value;
-  var quantityStr = document.getElementById('quantitys').value;
-  invRef.child(key).on("value", snap => {
-      var name = snap.child("Name").val();
-      var price = snap.child("Price").val();
-  	var subtotal = price*quantityStr;
-  	sum = sum + subtotal;
-    row++;
-      $("#bill").append("<tr id=bill-"+row+"><td>" + name + "</td><td>"+price+"</td><td>"+ quantityStr +"</td><td>"+subtotal+"</td><td><button type='button' class='btn btn-primary btn-sm' onclick='deleteRow("+row+");'>x</button></tr>");
-  	document.getElementById('total').innerHTML = sum;
-  });
+  var quantityStr = parseInt(document.getElementById('quantitys').value);
+  var iShop = document.getElementById("ShopID").value;
+invRef.child(key).on("value", snap => {
+    var name = snap.child("Name").val();
+    var price = parseInt(snap.child("Price").val());
+	var quantityxx = parseInt(snap.child("Shop").child(iShop).val());
+	var i,code,count=0;
+	for (i=1 ; i <= row ; i++){
+		code = document.getElementById("bill-"+i+"").getAttribute("name");
+		if (code===key){
+			count = i;
+		}
+	}
+	if (count == 0)
+	{
+		if(quantityxx >= quantityStr)
+		{
+			var subtotal = price*quantityStr;
+			row++;
+			$("#bill").append("<tr name="+key+" id=bill-"+row+"><td>" + name + "</td><td>"+price+"</td><td>"+ quantityStr +"</td><td>"+subtotal+"</td><td><button type='button' class='btn btn-primary btn-sm' onclick='deleteRow("+row+");'>x</button></tr>");
+		}else{
+			alert("Error! Out of stock!");
+		}
+	} else {
+		var oldquantity = parseInt($("#bill tr:eq("+count+") td:eq("+2+")").html());
+		quantityStr = quantityStr + oldquantity;
+		if(quantityxx >= quantityStr)
+		{
+			var subtotal = price*quantityStr;
+			var output = parseInt($("#bill tr:eq("+count+") td:eq("+3+")").html());
+			deleteRow(count);
+			row++;
+			$('#bill > thead > tr').eq(count-1).after("<tr name="+key+" id=bill-"+count+"><td>" + name + "</td><td>"+price+"</td><td>"+ quantityStr +"</td><td>"+subtotal+"</td><td><button type='button' class='btn btn-primary btn-sm' onclick='deleteRow("+row+");'>x</button></tr>");
+		}else{
+			alert("Error! Out of stock!");
+		}
+	}
+
+});
 }
 
 function deleteRow(id){
+  var output = parseInt($("#bill tr:eq("+id+") td:eq("+3+")").html());
   $('#bill-'+id).remove();
+  row--;
+  sum = 0;
+}
+
+  function fillcode(e){
+    var currentRow = $(e).closest('tr');
+      alert(currentRow.id);
+  }
+
+function commit(){
+	var i,  code, name , subtotal, quantity;
+	var d = new Date();
+	var year = d.getFullYear();
+	var month = d.getMonth()+1;
+	var day = d.getDate();
+	var x = document.getElementById("bill").rows.length;
+	for (i = 1; i < x; i++)
+	{
+		code = document.getElementById("bill-"+i+"").getAttribute("name");
+		name = $("#bill tr:eq("+i+") td:eq("+0+")").html();
+		quantity = parseInt($("#bill tr:eq("+i+") td:eq("+2+")").html());
+		subtotal = parseInt($("#bill tr:eq("+i+") td:eq("+3+")").html());
+		sum = sum + subtotal;
+	}
+	items = x-1;
+	alert("The total amount : " + sum +" $"+"\nThe number of sold items: " + items);
 }
 
 function deleteFun(){
@@ -100,6 +175,6 @@ function deleteFun(){
     for (i=1;i<x;i++){
         document.getElementById("bill").deleteRow(1);
     }
-	sum=0;
-	document.getElementById('total').innerHTML = sum;
+	sum = 0;
+	row = 0;
 }
