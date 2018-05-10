5f23b32f9de166112d51afa7f02a9fc5f93ee1ba
Tonywolf1997 <39082257+Tonywolf1997@users.noreply.github.com>
2018-05-10 17:22:08 +0700

    Add database script

--- public/js/index.js
+++ public/js/index.js
@@ -34,11 +34,6 @@ firebase.auth().onAuthStateChanged(function(user) {
 });
 
 
-
-
-
-
-
 function logoutClick(){
   firebase.auth().signOut();
 }
@@ -72,8 +67,8 @@ function createShop(){
 }
 // Add products
 
-
-var invRef = firebase.database().ref().child("Items");
+var rootRef = firebase.database();
+var invRef = rootRef.ref().child("Items");
 function submitClick() {
   var iCode = document.getElementById("barcode").value;
   var iName = document.getElementById("itemName").value;
@@ -84,13 +79,12 @@ function submitClick() {
 	invRef.child(iCode).update({
 	  Code: iCode,
     Name: iName,
-  	Price: iPrice,
-  	Cost: iCost
+  	Price: parseInt(iPrice),
+  	Cost: parseInt(iCost)
   });
-  invRef.child(iCode).child("Shop").child(iShop).set(iQuantity);
+  invRef.child(iCode).child("Shop").child(iShop).set(parseInt(iQuantity));
 }
 // Display products
-
 
 
 var items;
@@ -119,7 +113,7 @@ invRef.child(key).on("value", snap => {
 			row++;
 			$("#bill").append("<tr name="+key+" id=bill-"+row+"><td>" + name + "</td><td>"+price+"</td><td>"+ quantityStr +"</td><td>"+subtotal+"</td><td><button type='button' class='btn btn-primary btn-sm' onclick='deleteRow("+row+");'>x</button></tr>");
 		}else{
-			alert("Error! Out of stock!");
+		alert("Error!");
 		}
 	} else {
 		var oldquantity = parseInt($("#bill tr:eq("+count+") td:eq("+2+")").html());
@@ -129,45 +123,70 @@ invRef.child(key).on("value", snap => {
 			var subtotal = price*quantityStr;
 			var output = parseInt($("#bill tr:eq("+count+") td:eq("+3+")").html());
 			deleteRow(count);
+			if(row === count-1){
+				$('#bill').append("<tr name="+key+" id=bill-"+count+"><td>" + name + "</td><td>"+price+"</td><td>"+ quantityStr +"</td><td>"+subtotal+"</td><td><button type='button' class='btn btn-primary btn-sm' onclick='deleteRow("+count+");'>x</button></tr>");
+			}else{
+				$('#bill > thead > tr').eq(count-1).after("<tr name="+key+" id=bill-"+count+"><td>" + name + "</td><td>"+price+"</td><td>"+ quantityStr +"</td><td>"+subtotal+"</td><td><button type='button' class='btn btn-primary btn-sm' onclick='deleteRow("+count+");'>x</button></tr>");
+			}
 			row++;
-			$('#bill > thead > tr').eq(count-1).after("<tr name="+key+" id=bill-"+count+"><td>" + name + "</td><td>"+price+"</td><td>"+ quantityStr +"</td><td>"+subtotal+"</td><td><button type='button' class='btn btn-primary btn-sm' onclick='deleteRow("+row+");'>x</button></tr>");
 		}else{
-			alert("Error! Out of stock!");
+		alert("Error!");
 		}
 	}
-
 });
 }
 
 function deleteRow(id){
-  var output = parseInt($("#bill tr:eq("+id+") td:eq("+3+")").html());
   $('#bill-'+id).remove();
   row--;
   sum = 0;
 }
 
-  function fillcode(e){
-    var currentRow = $(e).closest('tr');
-      alert(currentRow.id);
-  }
-
 function commit(){
+  var iShop = document.getElementById("ShopID").value;
+  var qListi = [];
+	var qLista = [];
+	var qListv = [];
 	var i,  code, name , subtotal, quantity;
 	var d = new Date();
 	var year = d.getFullYear();
 	var month = d.getMonth()+1;
 	var day = d.getDate();
+	var DateId = year*10000+month*100+day;
+	console.log(DateId);
 	var x = document.getElementById("bill").rows.length;
 	for (i = 1; i < x; i++)
 	{
 		code = document.getElementById("bill-"+i+"").getAttribute("name");
+    qListi.push(code);
 		name = $("#bill tr:eq("+i+") td:eq("+0+")").html();
 		quantity = parseInt($("#bill tr:eq("+i+") td:eq("+2+")").html());
+    qLista.push(-quantity);
 		subtotal = parseInt($("#bill tr:eq("+i+") td:eq("+3+")").html());
+    qListv.push(subtotal);
 		sum = sum + subtotal;
 	}
 	items = x-1;
-	alert("The total amount : " + sum +" $"+"\nThe number of sold items: " + items);
+  document.getElementById('total').innerHTML = sum;
+  alert("The total amount : " + sum +" $"+"\nThe number of sold items: " + items);
+  var TransRef = rootRef.ref().child("Transaction");
+  var qId=TransRef.push();
+  qId.set({
+			Count: items,
+      Shop: iShop,
+			Time: DateId,
+			Item:{
+			}
+		});
+  for(i=0;i<=items;i++ ){
+	qId.child("Item/"+(i+1)).update({
+			Item: qListi[i],
+			Amount: qLista[i],
+			Value: qListv[i]});
+  	rootRef.ref('Items/' + qListi[i] + '/Shop/'+iShop).transaction(function(currentValue) {
+  			return (currentValue||0) + parseInt(qLista[i]); // +/- depend on manager/employee
+  	});
+	}
 }
 
 function deleteFun(){
@@ -177,4 +196,39 @@ function deleteFun(){
     }
 	sum = 0;
 	row = 0;
+}
+
+var ShowRef = rootRef.ref().child("Transaction");
+function createReport() {
+  var from = document.getElementById("from_field").value;
+  var to = document.getElementById("to_field").value;
+	ShowRef.orderByChild("Time").startAt(parseInt(from)).endAt(parseInt(to)).once('value',gotItem)
+}
+//Get Item
+function gotItem(data){
+	//console.log(data.val());
+	var Trans = data.val();
+	var keys = Object.keys(Trans);
+	//console.log(keys);
+	//for loop get Key Array
+	for (var i=0;i<keys.length;i++){
+		var k = keys[i];
+    var iShop = Trans[k].Shop;
+		var item = Trans[k].Item;
+    var date = parseInt(Trans[k].Time);
+    var D = date%100;
+    var M = (date-D) % 10000/100;
+    var Y = (date-M*100-D)/10000;
+		//console.log(item);
+		var count = Trans[k].Count;
+		//for loop append table
+		for(var j=1;j<=count;j++){
+			var stuff = item[j];
+			console.log(stuff);
+			var BarCode=stuff.Item;
+			var Amount=stuff.Amount;
+			var Value = stuff.Value;
+			$("#report_body").append("<tr><td>"+k+"</td><td>"+iShop+"</td><td>"+D+"/"+M+"/"+Y+"</td><td>"+BarCode+"</td><td>"+Amount+"</td><td>"+Value+"</td></tr>");
+		}
+	}
 }
